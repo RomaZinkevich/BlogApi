68ac7fc0801d3290899fa222cd9154a33adf3317
RomaZinkevich <zra1903@mail.ru>
2025-04-17 22:53:43 +0300

    created the application

---
+++ src/test/java/com/zirom/blog/BlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zirom.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
